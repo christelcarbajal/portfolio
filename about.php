17e37dad2539e4cec8720505c659668fab8493a7
Christel Carbajal <1011926@hr.nl>
2022-04-15 16:36:33 +0200

    Created about page (including banner, navbar)

---
+++ about.php
@@ -0,0 +1,78 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Creative.Dev</title>
+</head>
+<body>
+<header>
+    <img class="header-img" src="images/Banner-about.webp" alt="about header">
+</header>
+<main>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About Me</a>
+        <a href="projects.php">Projects</a>
+        <a href="cv.php">CV</a>
+        <a href="contact.php">Contact Me</a>
+    </nav>
+    <section>
+        <div>
+            <h1>Who <span style="color:#bdff38">am I</span>?</h1>
+        </div>
+        <p>
+            My name is Christel Carbajal Torres and I am currently 30 years old. I was born and raised in Curacao but my
+            roots are 100% Venezuelan.
+            I speak several languages such as Spanish, English, Dutch, Papiamento, a little Arabic and Portuguese.
+        </p>
+        <br>
+        <p>
+            Since I was a child, I have been a fan of video-games and game consoles.
+            And of the whole technological field, as a matter of fact. However, it wasn’t until 4 years ago that I
+            started to focus on developing games and programming.
+            The reason why I didn’t focus on programming earlier was that I decided to focus on science instead and.
+            Consequently - in 2016 – I graduated as an analyst in biotechnology and started working at Janssen Biologics
+            (a pharmaceutical company owned by Johnson & Johnson).
+        </p>
+        <br>
+        <p>
+            Because I am eager to learn new things, I challenged myself to proceed in programming. I started building my
+            own pc, configured my own Raspberry Pi with retro-games to add to my collection of (especially) console
+            games.
+            <br>
+            January of 2020 I started a Specialization in Coursera on “Game Design & Development” which contained 5
+            courses, of which I completed 3 already.
+            The other 2 are still pending, though, due to work and my studies. Since September 2020, namely, I have been
+            studying CMGT (Creative Media & Game Technologies) at Rotterdam University of Applied Science.
+            The reason I started this course was to gain more experience and knowledge as a starting programmer. There
+            is a lot to learn, and I just got started.
+            Before I started I had no idea of coding, and I guess I was a little afraid it wasn’t for me, but I decided
+            to
+            give it a shot.
+            And after having done some small projects, I quickly realized that this is where my heart is.
+        </p>
+        <br>
+        <h3>Why am I <span style="color:#bdff38">interested</span> in developing <span
+                    style="color:#bdff38">games</span>?</h3>
+        <p>
+            I developed my love for game development thanks to the Ubisoft company. I have always been such a fan of
+            most of the Ubisoft games, like Assassin’s Creed & Tom Clancy’s franchise.
+            I admire the spectacular idea of introducing a series of features, such as science fiction intertwined with
+            historical events in the real world, that was just amazing.
+            I loved the idea of learning history from a video game, so I questioned myself, what if I could learn new
+            things that way?
+            Could it be possible to make a video game where you could have fun but at the same time learn new things?
+            Imagine in the future children learning their subjects through a video game?
+            That would be amazing. So that is why I decided to choose this path to expand my knowledge and my creativity
+            in this field.
+        </p>
+    </section>
+</main>
+</body>
+</html>
