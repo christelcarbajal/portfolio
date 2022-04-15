f105b05f06958771aabc2f29536b768a63a0b0c6
Christel Carbajal <1011926@hr.nl>
2022-04-15 16:35:32 +0200

    Created homepage (including banner, navbar, footer)

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/8d8e63315b.js" crossorigin="anonymous"></script>
+    <title>Creative.Dev</title>
+</head>
+<div>
+    <header>
+        <img class="header-img" src="images/Banner-home.webp" alt="home header">
+    </header>
+    <main>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About Me</a>
+            <a href="projects.php">Projects</a>
+            <a href="cv.php">CV</a>
+            <a href="contact.php">Contact Me</a>
+        </nav>
+        <section>
+            <div class="banner-wrapper">
+                <div class="banner-content" alt="background picture">
+                    <div>
+                        <h1>Hi there! I'm <span style="color:#bdff38">Christel</span><br>
+                            I'm a <span style="color:#bdff38">web & game developer</span> based in <span
+                                    style="color:#bdff38">The Netherlands</span></h1>
+                    </div>
+                    <br>
+                    <div class="bio">
+                        <img class="bio-photo" src="images/chris.webp" alt="bio picture">
+                    </div>
+                    <div class="bio-text">
+                        <p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="icon">
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/christel-carbajal-torres/"><i
+                                class="fa-brands fa-linkedin"></i></a><br>
+                </li>
+                <li>
+                    <a href="https://github.com/christelcarbajal"><i class="fa-brands fa-github-square"></i></a><br>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/creative.dev02/"><i
+                                class="fa-brands fa-instagram-square"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCmPh8Uo8-knENq_OR0W9sSg"><i
+                                class="fa-brands fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
