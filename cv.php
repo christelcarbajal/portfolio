9b57edc4ac576bccd61808a76214709ed91ddaea
Christel Carbajal <1011926@hr.nl>
2022-04-16 23:43:21 +0200

    Created cv page (with downloadable cv)

---
+++ cv.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Creative.Dev</title>
+</head>
+<header>
+    <img class="header-img" src="images/Banner-cv.webp" alt="cv header">
+</header>
+<main>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About Me</a>
+        <a href="projects.php">Projects</a>
+        <a href="cv.php">CV</a>
+        <a href="contact.php">Contact Me</a>
+    </nav>
+    <section>
+        <div>
+            <h3>My <span style="color:#bdff38">Curriculum</span> Vitae</h3>
+            <p><a href="cv/cv-gameDev.pdf"><span style="color:#ffcc01">Download CV</span></a></p>
+            <div class="cv">
+                <img src="cv/cv-gameDev01.png" alt="cv-part-one" style="width: 50vw">
+                <img src="cv/cv-gameDev02.png" alt="cv-part-two" style="width: 50vw">
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+</html>
